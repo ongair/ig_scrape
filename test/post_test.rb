74c102666392baa1a6d043f135986f5a1d769cd2
Trevor Kimenye <kimenye@gmail.com>
2017-09-23 09:37:52 +0300

    converting edge to normal payload

--- test/post_test.rb
+++ test/post_test.rb
@@ -51,4 +51,50 @@ describe "Posts" do
     assert_equal comment.author_name, "claudinhamarm"
     assert_equal comment.author_profile_pic, "https://instagram.com/a.jpg"
   end
+
+  it "can convert edge to standard post payload" do
+    edge_payload = {
+      "node" => {
+        "id" => "12345",
+        "__typename" => "GraphImage",
+        "edge_media_to_caption" => {
+          "edges"=> [
+            {
+              "node" => {
+                "text" => "Caption"
+              }
+            }
+          ]
+        },
+        "shortcode" => "BZF6YwmFSb1",
+        "edge_media_to_comment" => {
+          "count" => 15
+        },
+        "edge_media_preview_like" => {
+          "count" => 10
+        },
+        "taken_at_timestamp" => 1234890,
+        "display_url" => "https://instagram.fnbo2-1.fna.fbcdn.net/test.jpg",
+        "is_video" => false
+      }
+    }
+
+    payload = {
+      "id" => "12345",
+      "__typename" => "GraphImage",
+      "is_video" => false,
+      "code" => "BZF6YwmFSb1",
+      "display_src" => "https://instagram.fnbo2-1.fna.fbcdn.net/test.jpg",
+      "caption" => "Caption",
+      "date" => 1234890,
+      "comments" => {
+        "count" => 15
+      },
+      "likes" => {
+        "count" => 10
+      }
+    }
+
+    assert_equal IGScrape::Post.edge_timeline_to_payload(edge_payload), payload
+  end
 end
